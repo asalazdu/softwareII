f4654b3f495ed72a0ad689a3e4d551552ded6d72
Alexis <alexizarduque@gmail.com>
2021-09-27 21:36:03 -0500

    Implementación de la interfaz

--- src/main/java/net/software/backendcursojava/services/UserService.java
+++ src/main/java/net/software/backendcursojava/services/UserService.java
@@ -1,16 +1,34 @@
 package net.software.backendcursojava.services;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import net.software.backendcursojava.UserRepository;
+import net.software.backendcursojava.entities.UserEntity;
 import net.software.backendcursojava.shared.dto.UserDTO;
 
 @Service
 public class UserService implements UserServiceinterface{
 
+    @Autowired
+    UserRepository userRepository;
+
     @Override
     public UserDTO createUser(UserDTO userDTO) {
-        // TODO logica de la creacion del usuario 
-        return null;
+        //TODO logica de la creacion del usuario 
+
+        UserEntity userEntity = new UserEntity();
+        BeanUtils.copyProperties(userDTO, userEntity);
+
+        userEntity.setEncryptedPassword("testpassword");
+        userEntity.setUserId("testUserid");
+
+        UserEntity storedUserDetail = userRepository.save(userEntity);
+
+        UserDTO userToReturn = new UserDTO();
+        BeanUtils.copyProperties(storedUserDetail, userToReturn);
+        return userToReturn;
     }
 
     
